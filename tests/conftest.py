b4dd96c4ee07d6b5348effb613537f3797c2b3a8
Pavel Polezhaev <pavelpoleczaev@gmail.com>
2024-08-05 21:56:16 +0300

    add classes product and categorie

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src.category_class import Category
+from src.product_class import Product
+
+
+@pytest.fixture()
+def products_iphon():
+    return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+
+@pytest.fixture()
+def class_smartphon():
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
